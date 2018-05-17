2f9335ec0a8d71845e3cf1958101fe3b28ce92be
AurhinDev <jacobbrickane@gmail.com>
2018-05-17 20:57:38 +0200

    Updated texts

--- components/About.vue
+++ components/About.vue
@@ -3,15 +3,10 @@
 		<div class="row">
 			<div class="col-10 push-1">
 				<p>
-					A graduate of Game Development with 2 years of professional experience
-					in a diverse career, from an indie game studio with less than a handful 
-					employees to a gargantuan telecom giant. I have a wide variety of skills
-					and spend a fair share of my spare time to refining those.
+					A graduate of Game Development with 2 years of professional experience in a variety of working environments - from a small indie game studio with less than a handful employees, to a gargantuan telecom giant. I have a wide array of skills and spend a fair share of my spare time refining them.
 				</p>
 				<p>
-					I'm always looking forward to meeting new people and to learn more as I
-					help them solve their problems in areas related to cryptocurrencies & blockchain,
-					game design, project management or software development.
+					I'm always looking forward to meeting new people and to learn more as I help them solve their problems in areas related to cryptocurrencies and blockchain, game design, project management or software development.
 				</p>
 			</div>
 		</div>
--- components/Skills.vue
+++ components/Skills.vue
@@ -12,12 +12,7 @@
 						</h3>
 						<hr class="dash">
 						<p>
-							Are one of those who are lost among all these currencies? I offer
-							guidence to help you navigate and reduce the risk of your investments.
-							By listening to your needs and determining your risk profile, I will 
-							supply you by a portfolio that fit your needs. I have years of experience
-							in the investment market and use a combination of technical and fundamental 
-							analysis aswell as a stop-loss strategy to help you maximize your gains!
+							Are you one of those who feel a bit lost when wandering among all these new currencies? I offer guidance to help you navigate and can also supply you with advise to reduce the risk of your investments. By listening to your needs and determining your risk profile I will offer you a portfolio that fits your needs. I have years of experience in the investment market and use a combination of technical and fundamental analysis, as well as a stop-loss strategy, to help you maximize your gains.
 						</p>
 					</div>
 					<div class="col-12 such-smooth">
@@ -29,12 +24,7 @@
 						</h3>
 						<hr class="dash">
 						<p>
-							I'm a Game Designer since early childhood who started tweaking
-							play in order to experiment with rules. This combined with a mathematical mindset,
-							3 years of University Education and more than 10 games I make up for a great 
-							game designer. By establishing your demands and determining the target audience
-							I will bring a mixture of hard analyze and refreshing creativity to your project
-							that may make you reconsider certain decisions.
+							I'm a Game Designer since early childhood, a time when I started tweaking games in order to experiment with rules. This, combined with a mathematical mindset, 3 years of University Education, and more than 10 created games, has with time made me a great game designer. By establishing your demands and determining the target audience, I will bring a mixture of hard analysis and refreshing creativity to your project that may make you reconsider certain decisions.
 						</p>
 					</div>
 					<div class="col-12 such-smooth">
@@ -46,13 +36,7 @@
 						</h3>
 						<hr class="dash">
 						<p>
-							I have experience of managing four projects form start to finish. With indepth
-							knowledge of group psychology and leadership and familarity with several 
-							management principles (waterfall, gannt, agile, scrum), I will lead your team towards your goal. 
-							Starting of by considering all aspects of implementation and determining timeframe and deadlines we will
-							move towards understanding the emplooyes most productive areas and put the plan into action.
-							I'm also able to deliver on weekly/monthly reconciliation to further ensure that the project
-							is progressing accordingly.
+							I have experience of managing four projects form start to finish. With in-depth knowledge of group psychology and leadership, as well as familarity with several management principles (waterfall, gannt, agile, scrum), I will lead your team until we reach your envisioned goal. Starting off by considering all aspects of implementation and determining timeframe and deadlines, we will move towards understanding the employees' most productive areas. From there, we can successfully put the plan into action. I'm also able to deliver on weekly/monthly reconciliation to further ensure that the project is progressing accordingly.
 						</p>
 					</div>
 					<div class="col-12 such-smooth">
@@ -64,11 +48,7 @@
 						</h3>
 						<hr class="dash">
 						<p>
-							I have 5 years of programming experience where I have worked with everything inbetween game development
-							to devops to blockchain technology. I can help you team with your game, website or desktop applications
-							in a variety of languages and frameworks. I also have expertise in creating ICOs/Crowdfunds and DApps.
-							Whether you hire me to implement some new functionality, refactor your codebase or just to start a project from 
-							scratch is up to you. 
+							I have 5 years of programming experience where I have worked with everything from game development to devops and blockchain technology. I can help your team with your game, website or desktop applications in a variety of languages and frameworks. I also have expertise in creating ICOs/Crowdfunds and DApps. Whether you hire me to implement some new functionality, refactor your codebase or just to start a project from scratch, is up to you.
 						</p>
 					</div>
 				</div>
